3704456f2cb19ed3ee0263c3745ba9a228a511f8
Brushett Lab <brushlab@mit.edu>
2019-10-03 16:22:55 -0400

    Removed unnecessary import

--- analysis.py
+++ analysis.py
@@ -23,9 +23,6 @@ import numpy as np
 import scipy as scipy
 import scipy.optimize as opt
 
-#File handling
-import sys
-
 function, image, *kwargs = sys.argv
 
 if sys.platform == 'linux' and 'home' in image and image[0] != '/':
